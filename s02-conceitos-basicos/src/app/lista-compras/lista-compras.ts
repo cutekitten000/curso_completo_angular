2ae6529afbcc737dca6388e5a0e2daded16d8b0d
Danilo Aguiar <79465051+cutekitten001@users.noreply.github.com>
2025-05-31 16:21:18 +0000

    aula010: trabalhando com classe de modelo e arrays

--- s02-conceitos-basicos/src/app/lista-compras/lista-compras.ts
+++ s02-conceitos-basicos/src/app/lista-compras/lista-compras.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './item-lista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -10,9 +11,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class ListaCompras {
   item: string = "";
+  lista: ItemLista[] = [];
 
   adicionarItem() {
-    this.item = this.item;
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista);
   }
 }
